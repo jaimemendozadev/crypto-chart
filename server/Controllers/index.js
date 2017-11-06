f4b25482bb983d6320e98001190eca31053625c4
jaimemendozadev <jaimemendozadev@gmail.com>
2017-11-06 14:09:50 -0800

    Finish fetchpricedata route controller
    fetchData controller makes 2 api calls to fetch
    multiple cryptocurrency data and then resolve
    all promises in axios.spread.

--- server/Controllers/index.js
+++ server/Controllers/index.js
@@ -1,24 +1,62 @@
 const newCurrencyData = require('../utils.js').newCurrencyData;
 const moment = require('moment');
+const axios = require('axios');
+
+const currentDate = require('moment')();
+const BRAVECOIN_URL = process.env.BRAVECOIN_URL;
+const KEY = process.env.X_MASHAPE_KEY;
+const HEADERS = {headers: {"X-Mashape-Key": KEY, "Accept": "application/json"}};
+
+
+/*
+API URL Query string
+?
+coin=ETH &
+from=1451606400 &
+market=USD &
+to=1483189199
+
+*/
 
 
 
 var fetchData = (req, res) => {
-  //get current date, init variables
-  var currentDate = moment();
+  //init date variables
   var year = currentDate.year();
-  var monthInt, monthString, day, yearOfData; 
+  var beginOfYear = `1-1-${year}`;
+  var start = moment(beginOfYear, "M-D-YYYY").format('X');
+  var end = currentDate.format('X');
+
+
+
 
   //create data structure
   yearOfData = newCurrencyData(year);
 
 
-  //make api call here
+  var monthInt, monthString, day, yearOfData; 
 
-  console.log("BRAVECOIN_URL is ", BRAVECOIN_URL)
+  //make api calls here
+  var ETH = axios.get(`${BRAVECOIN_URL}?coin=ETH&from=${start}&market=USD&to=${end}`, HEADERS);
+  var BTC = axios.get(`${BRAVECOIN_URL}?coin=BTC&from=${start}&market=USD&to=${end}`, HEADERS);
+
+  axios.all([ETH, BTC])
+    .then(axios.spread( (eth, btc) => {
+      // Both requests are now complete
+      console.log("eth is ", eth);
+      console.log("\n");
+      console.log("btc is ", btc);
+
+      res.send("Inside axios spread, promises resolved!");
+
+    }))
+    .catch(error => {
+      console.log("The error inside axios spread is ", error);
+      res.send("There was a problem fetching the currency data in Axios spread.");
+    });
+
+
   
-  //axios.get()
-
     
 }
 
